7e00f9772559122b7bbaba0f19b839709dd6d7e9
Jennyfer Caucota <caucotafran@gmail.com>
2024-11-28 04:07:21 -0300

    28/11/24
    cambios

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,21 +1,5 @@
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-	const toggle = document.getElementById(toggleId),
-		  nav = document.getElementById(navId)
- 
-	toggle.addEventListener('click', () =>{
-		// Add show-menu class to nav menu
-		nav.classList.toggle('show-menu')
- 
-		// Add show-icon to show and hide the menu icon
-		toggle.classList.toggle('show-icon')
-	})
- }
- 
- showMenu('nav-toggle','nav-menu')
-
-
- document.addEventListener('scroll', function() {
+//whatsapp 
+document.addEventListener('scroll', function() {
     const whatsappContainer = document.getElementById('whatsapp-container');
     const footer = document.querySelector('footer'); // Suponiendo que tu footer tiene la etiqueta <footer>
 
