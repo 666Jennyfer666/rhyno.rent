35d4199f3dbc26cf69497cd28aedab1061394453
Jennyfer Caucota <caucotafran@gmail.com>
2024-11-30 20:26:15 -0300

    30/11/24
    añadí las paginas "sobre nosotros" y "contacto"

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,4 @@ const showMenu = (toggleId, navId) =>{
  showMenu('nav-toggle','nav-menu')
 
 
- document.addEventListener('scroll', function() {
-    const whatsappContainer = document.getElementById('whatsapp-container');
-    const footer = document.querySelector('footer'); // Suponiendo que tu footer tiene la etiqueta <footer>
 
-    // Calcula la distancia entre el botón y la parte inferior de la página
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    const windowHeight = window.innerHeight;
-    const footerTop = footer.getBoundingClientRect().top + scrollPosition;
-
-    // Ajusta la posición del botón de WhatsApp para que no se superponga con el footer
-    if (scrollPosition + windowHeight >= footerTop) {
-        const overlap = (scrollPosition + windowHeight) - footerTop;
-        whatsappContainer.style.bottom = `${5 + overlap}px`;
-    } else {
-        whatsappContainer.style.bottom = '2%'; // Valor predeterminado
-    }
-});
